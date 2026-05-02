dfa416cda40c810f9f1356a4eb4d8ae69bcebac4
FaustVX <jodu035@gmail.com>
2026-05-02 18:45:31 +0200

    Updated tasks & launch

--- Program.cs
+++ Program.cs
@@ -1,29 +1,8 @@
-﻿using System.Runtime.CompilerServices;
-using System.Text;
-using RecursiveParsing;
-using EBNF = EBNFParser;
+﻿using RecursiveParsing;
 
 // https://www.youtube.com/watch?v=SToUyjAsaFk
 // http://slebok.github.io/zoo/
 
 var input = (args is [var p,..] && File.Exists(p)) ? File.ReadAllText(p) : throw new Exception();
-
-// var ebnf = new EBNF.Phases.Parse.Parser(input).ParseFile();
-
-// var visitor0 = new EBNF.Visitors.CheckIdentifierVisitor(throwOnError: true);
-// ebnf.Accept(visitor0);
-// var visitor1 = new EBNF.Visitors.CSharpVisitor(nameof(RecursiveParsing), "Parser");
-// ebnf.Accept(visitor1);
-// PrintSB(visitor1.Parser);
-// PrintSB(visitor1.IVisitor);
-// PrintSB(visitor1.TreeNode);
-// PrintSB(visitor1.Token);
-// PrintSB(visitor1.Tokenizer);
 var ast = new Parser(input).ParseFile();
 ;
-
-static void PrintSB(StringBuilder sb, [CallerArgumentExpression(nameof(sb))]string expr = default!)
-{
-    Console.WriteLine($"- {expr}:");
-    Console.WriteLine(sb);
-}
