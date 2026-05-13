121fd51a50d3279ad693d7bf25d2e9453bc37f8d
FaustVX <jodu035@gmail.com>
2026-05-13 22:09:25 +0200

    Only parse `.txt` files

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@
 // https://www.youtube.com/watch?v=SToUyjAsaFk
 // http://slebok.github.io/zoo/
 
-var input = (args is [var p,..] && System.IO.File.Exists(p)) ? System.IO.File.ReadAllText(p) : throw new Exception();
+var input = (args is [var p,..] && new FileInfo(p) is { Exists: true, Extension: ".txt" }) ? System.IO.File.ReadAllText(p) : throw new Exception();
 var ast = new Parser(input).ParseFile();
 ;
