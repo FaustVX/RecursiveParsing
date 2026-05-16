25efe6217bc57e5f40d60cccf82a0309e857f2f9
FaustVX <jodu035@gmail.com>
2026-05-16 22:59:15 +0200

    Updated lib & added error printing

--- Ext.cs
+++ Ext.cs
@@ -2,28 +2,8 @@ using System.Text;
 
 namespace RecursiveParsing;
 
-static class Ext
+partial class Ext
 {
-    extension<T>(ReadOnlyMemory<T> rom)
-    where T : struct
-    {
-        public T? First
-        {
-            get
-            {
-                if (rom.IsEmpty)
-                    return null;
-                return rom.Span[0];
-            }
-        }
-    }
-    extension<T>(ReadOnlyMemory<T>)
-    {
-        public static ReadOnlyMemory<T> operator ++(ReadOnlyMemory<T> rom)
-        => rom += 1;
-        public static ReadOnlyMemory<T> operator +(ReadOnlyMemory<T> rom, Index offset)
-        => rom[offset..];
-    }
     extension(StringBuilder sb)
     {
         public StringBuilder AppendLineLinux(string text)
--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
 ﻿using System.Text;
+using RecursiveParsing;
 using RecursiveParsing.Parse;
+using RecursiveParsing.Tokenize;
 using RecursiveParsing.Visitors;
 
 // https://www.youtube.com/watch?v=SToUyjAsaFk
 // http://slebok.github.io/zoo/
 
-var input = (args is [var p,..] && new FileInfo(p) is { Exists: true, Extension: ".txt" }) ? System.IO.File.ReadAllText(p) : throw new Exception();
-var ast = new Parser(input).ParseFile();
-// ast.Accept(new TreePrintVisitor(input.AsMemory()));
-var qbe = new QBEVisitor();
-ast.Accept(qbe);
-System.IO.File.WriteAllBytes(@"obj/main.ssa", Encoding.ASCII.GetBytes(qbe.QBEFile.ToString()));
-;
+var input = (args is [var p,..] && new FileInfo(p) is { Exists: true, Extension: ".txt", FullName: var f }) ? System.IO.File.ReadAllText(f) : throw new Exception();
+try
+{
+    var ast = new Parser(input).ParseFile();
+    // ast.Accept(new TreePrintVisitor(input.AsMemory()));
+    var qbe = new QBEVisitor();
+    ast.Accept(qbe);
+    System.IO.File.WriteAllBytes(@"obj/main.ssa", Encoding.ASCII.GetBytes(qbe.QBEFile.ToString()));
+}
+catch (EBNFException ex)
+{
+    var (l, c) = ex.Range.Start.GetPos(input);
+    Console.WriteLine($"[{f}:{l}:{c}]: {ex.SubCategory} {ex.ErrorCode}: {ex.Message}");
+    return 1;
+}
+return 0;
